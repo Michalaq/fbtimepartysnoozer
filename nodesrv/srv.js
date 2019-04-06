f039624ef4f9321b10e72fe3ff1690eb968973a1
Kamil Braun <kb346840@students.mimuw.edu.pl>
2019-04-06 15:21:22 +0200

    func

--- nodesrv/srv.js
+++ nodesrv/srv.js
@@ -1,17 +1,19 @@
 var request = require('request');
 
-var options = {
-    uri: 'http://localhost:5000/answer',
-    method: 'POST',
-    json: {
-        "test": "test"
-    }
-};
+function ans(j) {
+    var options = {
+        uri: 'http://localhost:5000/answer',
+        method: 'POST',
+        json: j
+    };
 
-request(options, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body);
-  } else {
-      console.log("err " + error + ", " + response.statusCode);
-  }
-});
+    request(options, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log(body);
+      } else {
+          console.log("err " + error + ", " + response.statusCode);
+      }
+    });
+}
+
+ans({"test": "test"});
