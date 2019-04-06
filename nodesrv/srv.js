b908f069f8608aa5319eb243d6f479d20a45b800
Kamil Braun <kb346840@students.mimuw.edu.pl>
2019-04-07 00:56:23 +0200

    lastusers

--- nodesrv/srv.js
+++ nodesrv/srv.js
@@ -60,24 +60,35 @@ function Srv(onLogin) {
     this.api = null;
     this.startedYet = false;
 
-    this.suggest = function(id, onMsg) {
-        this.api.getThreadHistory(id, 10, undefined, (err, history) => {
-            if (err) return console.error(err);
+    this.suggest = function(id, onMsg, onFail) {
+        this.api.getThreadList(10, null, [], (err, list) => {
+            for (var i = 0; i < list.length; ++i) {
+                if (id == list[i].threadID) {
+                    return this.api.getThreadHistory(id, 10, undefined, (err, history) => {
+                        if (err) return console.error(err);
 
-            // console.log("history:");
-            // console.log(history);
+                        // console.log("history:");
+                        // console.log(history);
 
-            ans({"history": history, "myId": this.myId} , (body) => {
-                // console.log("processed:")
-                // console.log(body.processed);
+                        ans({"history": history, "myId": this.myId} , (body) => {
+                            // console.log("processed:")
+                            // console.log(body.processed);
 
-                d = body.ans.toString();
-                onMsg(d);
-            });
+                            d = body.ans.toString();
+                            onMsg(d);
+                        });
+                    });
+                }
+            }
+
+            var failMsg = "No such threadID";
+            console.log(failMsg);
+            onFail(failMsg);
         });
+
     };
 
-    this.sendTo = function(id, onSend) {
+    this.sendTo = function(id, onSend, onFail) {
         this.suggest(id, (msg) => {
             console.log("sending: " + d);
             this.api.sendMessage(d, id);
@@ -85,7 +96,7 @@ function Srv(onLogin) {
             if (onSend) {
                 onSend(d);
             }
-        });
+        }, onFail);
     };
 
     this.snooze = function(id, now) {
@@ -150,7 +161,11 @@ app.post('/send/:userId', (req, res) => {
         return;
     }
 
-    srv.sendTo(req.params.userId, (msg) => res.send("Sent: " + msg + "\n"));
+    srv.sendTo(
+        req.params.userId,
+        (msg) => res.send("Sent: " + msg + "\n"),
+        (msg) => res.send("Failed: " + msg + "\n")
+    );
 });
 
 app.get('/suggest/:userId', (req, res) => {
@@ -159,7 +174,36 @@ app.get('/suggest/:userId', (req, res) => {
         return;
     }
 
-    srv.suggest(req.params.userId, (msg) => res.send(msg + "\n"));
+    srv.suggest(
+        req.params.userId,
+        (msg) => res.send(msg + "\n"),
+        (msg) => res.send("Failed: " + msg + "\n")
+    );
+});
+
+app.get('/lastUsers', (req, res) => {
+    if (!srv.startedYet) {
+        res.status(503).send(notStartedMsg);
+        return;
+    }
+
+    srv.api.getThreadList(15, null, [], (err, list) => {
+        users = [].concat.apply([],
+            list.filter((t) => { return !t.isGroup; })
+                .map((t) => { return t.participants.filter((p) => { return p.accountType == "User"; }); }))
+                .filter((p) => { return p.userID != srv.myId; });
+
+        console.log(users);
+
+        res.send(users.map((u) => {
+            return {
+                "userID": u.userID,
+                "pic": u.profilePicture,
+                "name": u.name,
+                "snoozed": srv.snoozedOn.has(u.userID)
+            };
+        }));
+    });
 });
 
 app.listen(3000);
